81133f64e949f037b13f1d81269ea8c05c287e5a
dev4ult <nibrasbiasaaja@gmail.com>
2023-11-05 23:25:50 +0700

    feat: add user and content entities for auto migration

--- utils/database.go
+++ utils/database.go
@@ -2,7 +2,9 @@ package utils
 
 import (
 	"raihpeduli/config"
+	"raihpeduli/features/content"
 	"raihpeduli/features/fundraise"
+	"raihpeduli/features/user"
 
 	"fmt"
 
@@ -26,5 +28,6 @@ func InitDB() *gorm.DB {
 }
 
 func migrate(db *gorm.DB) {
-	db.AutoMigrate(fundraise.Fundraise{})
+	db.AutoMigrate(user.User{})
+	db.AutoMigrate(fundraise.Fundraise{}, content.Content{})
 }
