85eacbae0debdc8170f7d8ab471a2bc28517d5f2
dev4ult <nibrasbiasaaja@gmail.com>
2023-11-08 23:28:21 +0700

    fix: set route for fundraise and remove content entities

--- routes/fundraise.go
+++ routes/fundraise.go
@@ -10,7 +10,9 @@ func Fundraises(e *echo.Echo, handler fundraise.Handler) {
 	fundraises := e.Group("/fundraises")
 
 	fundraises.GET("", handler.GetFundraises())
+	fundraises.POST("", handler.CreateFundraise())
 	
 	fundraises.GET("/:id", handler.FundraiseDetails())
+	fundraises.PUT("/:id", handler.UpdateFundraise())
 	fundraises.DELETE("/:id", handler.DeleteFundraise())
 }
