1027a5a1d9bb71dfd54fdf74c92b62fd1499b4f0
Bagus Ario Yudanto <bagusarioyudanto123@gmail.com>
2023-11-19 15:27:34 +0700

    fix: fixing validation

--- features/auth/dtos/request.go
+++ features/auth/dtos/request.go
@@ -1,19 +1,19 @@
 package dtos
 
 type RequestLogin struct {
-	Email    string `json:"email" form:"email" validation:"required"`
-	Password string `json:"password" form:"password" validation:"required"`
+	Email    string `json:"email" form:"email" validate:"required"`
+	Password string `json:"password" form:"password" validate:"required"`
 }
 
 type InputUser struct {
-	Fullname    string `json:"fullname" form:"fullname" validation:"required"`
-	Address     string `json:"address" form:"address" validation:"required"`
-	PhoneNumber string `json:"phone_number" form:"phone_number" validation:"required"`
-	Gender      string `json:"gender" form:"gender" validation:"required"`
-	Email       string `json:"email" form:"email" validation:"required"`
-	Password    string `json:"password" form:"password" validation:"required"`
+	Fullname    string `json:"fullname" form:"fullname" validate:"required"`
+	Address     string `json:"address" form:"address" validate:"required"`
+	PhoneNumber string `json:"phone_number" form:"phone_number" validate:"required"`
+	Gender      string `json:"gender" form:"gender" validate:"required"`
+	Email       string `json:"email" form:"email" validate:"required"`
+	Password    string `json:"password" form:"password" validate:"required"`
 }
 
 type ResendOTP struct {
-	Email string `json:"email" form:"email" validation:"required"`
+	Email string `json:"email" form:"email" validate:"required"`
 }
