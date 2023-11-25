c2895b7542c9c01fcf18157a8516b167b68b0295
dev4ult <nibrasbiasaaja@gmail.com>
2023-11-25 17:03:26 +0700

    fix: delete old file and insert a new one on update photo in fundraise

--- helpers/upload-file.go
+++ helpers/upload-file.go
@@ -47,3 +47,30 @@ func (uf *cloudStorage) UploadFile(file multipart.File, object string) error {
 
 	return nil
 }
+
+func (uf *cloudStorage) DeleteFile(object string) error {
+	ctx := context.Background()
+
+	ctx, cancel := context.WithTimeout(ctx, time.Second*50)
+	defer cancel()
+
+	client, err := storage.NewClient(ctx)
+
+	if err != nil {
+		return err
+	}
+
+	o := client.Bucket(uf.bucketName).Object(uf.uploadPath + object)
+
+	attrs, err := o.Attrs(ctx)
+	if err != nil {
+			return fmt.Errorf("object.Attrs: %w", err)
+	}
+	o = o.If(storage.Conditions{GenerationMatch: attrs.Generation})
+
+	if err := o.Delete(ctx); err != nil {
+			return fmt.Errorf("Object(%q).Delete: %w", object, err)
+	}
+
+	return nil
+}
