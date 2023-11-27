d29a8900230ee92cc4eae1e9b2b592fcef178847
dev4ult <nibrasbiasaaja@gmail.com>
2023-11-27 14:36:15 +0700

    improve: integrate chatbot

--- features/chatbot/usecase/service.go
+++ features/chatbot/usecase/service.go
@@ -14,12 +14,14 @@ import (
 type service struct {
 	model chatbot.Repository
 	validation helpers.ValidationInterface
+	openAI helpers.OpenAIInterface
 }
 
-func New(model chatbot.Repository, validation helpers.ValidationInterface) chatbot.Usecase {
+func New(model chatbot.Repository, validation helpers.ValidationInterface, openAI helpers.OpenAIInterface) chatbot.Usecase {
 	return &service {
 		model: model,
 		validation: validation,
+		openAI: openAI,
 	}
 }
 
@@ -51,7 +53,9 @@ func (svc *service) SetReplyMessage(input dtos.InputMessage, userID int) (*dtos.
 		return nil, errMap, errors.New("message must not be empty") 
 	}
 
-	_, err := svc.model.ReadQuestionNPrompts()
+	// _, err := svc.model.ReadQuestionNPrompts()
+	
+	reply, err := svc.openAI.GetReplyFromGPT(input.Message, map[string]string{})
 
 	if err != nil {
 		return nil, nil, err
@@ -59,7 +63,7 @@ func (svc *service) SetReplyMessage(input dtos.InputMessage, userID int) (*dtos.
 
 	var chatMessage = chatbot.QuestionAndReply{
 		Question: input.Message,
-		Reply: "test",
+		Reply: reply,
 	}
 	
 	if userID != 0 {
@@ -70,7 +74,7 @@ func (svc *service) SetReplyMessage(input dtos.InputMessage, userID int) (*dtos.
 
 	var res = dtos.ResChatReply{
 		Question: input.Message,
-		Reply: "test",
+		Reply: reply,
 	}
 
 	return &res, nil, nil
