30034645eeb71cc9c5ee4f64e6324d88c97210a7
Bagus Ario Yudanto <bagusarioyudanto123@gmail.com>
2023-11-29 22:39:50 +0700

    bugfix: fixed validation at fullname field in register user

--- helpers/request-validation.go
+++ helpers/request-validation.go
@@ -1,6 +1,7 @@
 package helpers
 
 import (
+	"regexp"
 	"strings"
 
 	"github.com/go-playground/validator/v10"
@@ -14,6 +15,11 @@ func NewValidationRequest() ValidationInterface {
 
 func (v validation) ValidateRequest(request any) []string {
 	var validate = validator.New()
+
+	validate.RegisterValidation("fullname", func(fl validator.FieldLevel) bool {
+		regex := regexp.MustCompile("^[a-zA-Z ]+$")
+		return regex.MatchString(fl.Field().String())
+	})
 
 	if err := validate.Struct(request); err != nil {
 		var errMap = []string{}
