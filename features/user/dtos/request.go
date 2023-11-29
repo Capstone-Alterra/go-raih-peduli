1e8588490c8f347cc6bc0066120cdb4960081342
Irvan Wijaya <irfanwijayasardam@gmail.com>
2023-11-29 10:58:13 +0700

    recfractor : add input validation for users collection

--- features/user/dtos/request.go
+++ features/user/dtos/request.go
@@ -3,20 +3,20 @@ package dtos
 import "mime/multipart"
 
 type InputUser struct {
-	Fullname    string `json:"fullname" form:"fullname" validate:"required"`
+	Fullname    string `json:"fullname" form:"fullname" validate:"required,alpha"`
 	Address     string `json:"address" form:"address" validate:"required"`
-	PhoneNumber string `json:"phone_number" form:"phone_number" validate:"required"`
+	PhoneNumber string `json:"phone_number" form:"phone_number" validate:"required,min=10,max=13"`
 	Gender      string `json:"gender" form:"gender" validate:"required"`
-	Email       string `json:"email" form:"email" validate:"required"`
-	Password    string `json:"password" form:"password" validate:"required"`
+	Email       string `json:"email" form:"email" validate:"required,email"`
+	Password    string `json:"password" form:"password" validate:"required,min=8"`
 }
 
 type InputUpdate struct {
-	Fullname       string `json:"fullname" form:"fullname" validate:"required"`
+	Fullname       string `json:"fullname" form:"fullname" validate:"required,alpha"`
 	Address        string `json:"address" form:"address" validate:"required"`
-	PhoneNumber    string `json:"phone_number" form:"phone_number" validate:"required"`
+	PhoneNumber    string `json:"phone_number" form:"phone_number" validate:"required,min=10,max=13"`
 	Gender         string `json:"gender" form:"gender" validate:"required"`
-	Email          string `json:"email" form:"email" validate:"required"`
+	Email          string `json:"email" form:"email" validate:"required,email"`
 	ProfilePicture string `json:"profile_picture" form:"profile_picture"`
 }
 
@@ -34,10 +34,10 @@ type VerifyOTP struct {
 }
 
 type ForgetPassword struct {
-	Email string `json:"email" form:"email" validate:"email"`
+	Email string `json:"email" form:"email" validate:"required,email"`
 }
 
 type ResetPassword struct {
-	Email    string `json:"email" form:"email" validate:"required"`
-	Password string `json:"password" form:"password" validate:"required"`
+	Email    string `json:"email" form:"email" validate:"required,email"`
+	Password string `json:"password" form:"password" validate:"required,min=8"`
 }
