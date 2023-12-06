37611efa69954527922ef7ac74f6df1ad646bedd
Bagus Ario Yudanto <bagusarioyudanto123@gmail.com>
2023-12-06 11:57:31 +0700

    fix: fixed password validation

--- features/auth/dtos/request.go
+++ features/auth/dtos/request.go
@@ -2,7 +2,7 @@ package dtos
 
 type RequestLogin struct {
 	Email    string `json:"email" form:"email" validate:"required,email"`
-	Password string `json:"password" form:"password" validate:"required,alphanum,min=8"`
+	Password string `json:"password" form:"password" validate:"required,min=8"`
 }
 
 type InputUser struct {
@@ -11,7 +11,7 @@ type InputUser struct {
 	PhoneNumber string `json:"phone_number" form:"phone_number" validate:"required,number,min=10,max=13"`
 	Gender      string `json:"gender" form:"gender" validate:"required"`
 	Email       string `json:"email" form:"email" validate:"required,email"`
-	Password    string `json:"password" form:"password" validate:"required,alphanum,min=8"`
+	Password    string `json:"password" form:"password" validate:"required,min=8"`
 }
 
 type ResendOTP struct {
